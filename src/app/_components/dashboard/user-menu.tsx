5c50276162faed8226e7a9d86b2d72be958a0915
outsidermm <71582657+outsidermm@users.noreply.github.com>
2026-02-15 15:30:49 +1100

    fix: redirect to homepage

--- src/app/_components/dashboard/user-menu.tsx
+++ src/app/_components/dashboard/user-menu.tsx
@@ -184,7 +184,7 @@ export function UserMenu({ user }: UserMenuProps) {
             </button>
 
             <button
-              onClick={() => signOut({ callbackUrl: "/login" })}
+              onClick={() => signOut({ callbackUrl: "/" })}
               className="flex w-full items-center px-1.5 py-2.5 text-sm text-gray-700 hover:bg-gray-50"
             >
               <LogoutIcon className="mr-3 h-5 w-5 text-gray-500" />
