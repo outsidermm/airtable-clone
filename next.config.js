a30d4b28e10d7766776d1de125ba268e6c1c6b2f
outsidermm <71582657+outsidermm@users.noreply.github.com>
2026-02-15 18:58:17 +1100

    chore: refactor away from deprecating image config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,20 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     images:{
-        domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+                {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
     }
 };
 
