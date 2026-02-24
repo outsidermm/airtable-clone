35e842ebe71148daf5b235f4b128b0f3623b2e4b
outsidermm <71582657+outsidermm@users.noreply.github.com>
2026-02-24 21:12:03 +1100

    fix: added the missing dependency..

--- src/app/_components/base/hooks/useTableVirtualizer.ts
+++ src/app/_components/base/hooks/useTableVirtualizer.ts
@@ -99,7 +99,7 @@ export function useTableVirtualizer({
         el.addEventListener("scroll", onScroll, { passive: true });
         return () => el.removeEventListener("scroll", onScroll);
       },
-      [],
+      [totalVirtualHeight],
     ),
     scrollToFn: useCallback(
       (
